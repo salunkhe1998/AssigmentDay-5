a43da25929ba7039a3b762e0b5cf39487b364efd
Ashwini <salunkheashwini1998@gmail.com>
2023-03-29 15:48:04 +0530

    To find minimum integer value

---
+++ FindMinimum.cs
@@ -0,0 +1,30 @@
+﻿using GenericConcept;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericConcept
+{
+    public class FindMinimum
+    {
+        public static void IntMin()
+        {
+            int[] arr = DeleteArrayElement.GetIntElement();
+            for (int i = 0; i < arr.Length - 1; i++)
+            {
+                for (int j = 0; j < arr.Length - i - 1; j++)
+                {
+                    if (arr[j] > arr[j + 1])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Minimum value In Given Integer is " + arr[0]);
+        }
+    }
+}
