a6cca9a07316b94e845e210ef45ea0ca515881b8
salunkhe1998 <117974213+salunkhe1998@users.noreply.github.com>
2023-03-29 16:08:20 +0530

    method calling

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿namespace GenericConcept
+namespace GenericConcept
 {
     class Program
     {
@@ -7,8 +7,7 @@
             Console.WriteLine("Welcome To Generic Concepts Problems");
             Console.WriteLine("Select any one option:- \n1.Delete Integer,double,character Element from array." +
                               "\n2.Generic Method to Delete Integer,double,character Element from array." +
-                              "\n3.Generic Class to Delete Integer,double,character Element from array." +
-                              "\n4.Find Minimum Integer value.\n5.Find Minimum Double Value.\n6.Find Minimum string value.");
+                              "\n3.Generic Class to Delete Integer,double,character Element from array.");
             int option = Convert.ToInt32(Console.ReadLine());
 
             switch (option)
@@ -18,12 +17,12 @@
                     Console.WriteLine("Plese pick up any one element");
                     int delete = Convert.ToInt32(Console.ReadLine());
                     DeleteArrayElement.DeleteIntElement(intArry, delete);
-
+                    
                     double[] doubleArry = DeleteArrayElement.GetDoubleElement();
                     Console.WriteLine("Plese pick up any one element");
                     double doubleDelete = Convert.ToDouble(Console.ReadLine());
                     DeleteArrayElement.DeleteDoubleElement(doubleArry, doubleDelete);
-
+                  
                     char[] charArry = DeleteArrayElement.GetCharElement();
                     Console.WriteLine("Plese pick up any one element");
                     char charDelete = Convert.ToChar(Console.ReadLine());
@@ -34,12 +33,12 @@
                     Console.WriteLine("Plese pick up any one elemnt");
                     int deleteG = Convert.ToInt32(Console.ReadLine());
                     GenericMethod.DeleteElementFromArray<int>(intArryG, deleteG);
-
+                 
                     double[] doubleArryG = GenericMethod.GetDoubleElement();
                     Console.WriteLine("Plese pick up any one elemnt");
                     double doubleDeleteG = Convert.ToDouble(Console.ReadLine());
                     GenericMethod.DeleteElementFromArray<double>(doubleArryG, doubleDeleteG);
-
+                    
                     char[] charArryG = GenericMethod.GetCharElement();
                     Console.WriteLine("Plese pick up any one elemnt");
                     char charDeleteG = Convert.ToChar(Console.ReadLine());
@@ -57,21 +56,12 @@
                     double doubleDeleteGenericClass = Convert.ToDouble(Console.ReadLine());
                     GenericClass<double> deleteElementGeneric1 = new GenericClass<double>(doubleArryGenericClass, doubleDeleteGenericClass);
                     deleteElementGeneric1.RemoveElementFromGenericClass();
-
+                    
                     char[] charArryGenericClass = GenericMethod.GetCharElement();
                     Console.WriteLine("Plese pick up any one elemnt");
                     char charDeleteGenericClass = Convert.ToChar(Console.ReadLine());
                     GenericClass<char> deleteElementGeneric2 = new GenericClass<char>(charArryGenericClass, charDeleteGenericClass);
                     deleteElementGeneric2.RemoveElementFromGenericClass();
-                    break;
-                case 4:
-                    FindMinimum.IntMin();
-                    break;
-                case 5:
-                    FindMinimum.DoubleMin();
-                    break;
-                case 6:
-                    FindMinimum.StringMin();
                     break;
 
 
